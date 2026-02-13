c1b539961268bd233d38e0c958f60ae8ee564842
vc2mm4npwr-cyber <vc2mm4npwr@privaterelay.appleid.com>
2026-02-14 02:42:00 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,10 +5,11 @@ questions.forEach(function(question) {
   question.addEventListener('click', function() {
     const faqItem = question.closest('.faq-item');
     const answer = faqItem.querySelector('.answer');
-
     answer.classList.toggle('open');
   });
 });
+
+
 
 //===要素の取得===
 const openButton = document.getElementById('openModal');
@@ -26,12 +27,10 @@ closeButton.addEventListener('click', function(){
   modal.classList.remove('open');
 });
 
-//===背景クリックで閉じる===
-modal.addEventListener('click', function(){
-  modal.classList.remove('open');
-});
 
-//===中身クリックは無効化===
-modalContent.addEventListener('click', function(e){
-  e.stopPropagation();
+//===背景クリックで閉じる===
+modal.addEventListener('click',function(e)  {
+  if(e.target === modal) {
+    modal.classList.remove("open");
+  }
 });
